123ab74f35e3c5cecfd72b7c48631df43b7a3198
Markim Shaw <ms79723@gmail.com>
2021-04-01 02:59:27 -0500

    export types

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
+export { Product } from "./types/product";
+export { TopSearch } from "./types/top-search";
+export { NormalSearch } from "./types/normal-search";
+export { LiveSearch } from "./types/live-search";
+export { Category } from "./types/category";
 export { Book } from "./types/book";
